ee6214b0c72dbb7dd3be1f1fd85fa991da8371e0
EragonJ <eragonj@eragonj.me>
2014-08-05 00:18:28 +0800

    add dialog for hinting users about uploading process

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,18 @@ $(document).ready(function() {
   filepicker.setKey(apiKey);
 
   $('.upload').on('click', function() {
-    filepicker.pick();
+    filepicker.pickAndStore({
+      mimetype: 'image/*'
+    }, {}, function onSuccess() {
+      $('#successModal').modal();
+    }, function onError(error) {
+      // let's keep this insde body so that users don't have to
+      // worry how to report errors
+      var href =
+        'mailto:twobacksurfers@gmail.com?subject=Error Report&body=' +
+        JSON.stringify(error);
+      $('.error-report').attr('href', href);
+      $('#errorModal').modal();
+    });
   });
 });
